12cb1bd4e270377fad4fc8000e4d39a4b3ba0eaa
root <mao-nibori@gmo-k.jp>
2020-05-25 18:20:02 +0900

    heder commponent

---
+++ app/javascript/components/header_bottom.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="toggle_area">
+        <div class="toggle_button toggle_a toggled" v-on:click="$emit('toggle')">
+            <div class="toggle_text">{{ toggle }}</div>
+        </div>
+        <div class="toggle_button toggle_b">
+            <div class="toggle_text">おすすめ商品</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['toggle']
+}
+</script>
+
+<style lang="scss" scoped>
+.toggle_area {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    .toggle_button {
+        width: 50%;
+        height: 100%;
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #ccc;
+        font-size: 1.4rem;
+    }
+    .toggled {
+        color: #000;
+    }
+}
+</style>
