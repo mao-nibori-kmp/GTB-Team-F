b1ea2cebee93b74483978e9743d69e848c264b16
root <haruta-masaya@gmo-am.jp>
2020-05-26 10:41:51 +0900

    modified users_controller.rb and routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+    def index
+        
+    end
+
+    def new
+        @user = Users.new
+    end
+
+    def create
+        @user = Users.create(users_params)
+        redirect_to users_path
+    end
+
+    def edit
+        @user = Users.find(params[:id])
+    end
+
+    def update
+        @user = Users.find(params[:id])
+        @user.update(users_params)
+        redirect_to users_path
+    end
+
+    def destroy
+        @user = Users.find(params[:id])
+        @user.destroy
+        redirect_to users_path
+    end
+
+    private
+    def users_params
+        params.require(:user).permit(:mail, :nickname, :sex, :age, :deposit, :password_digest)
+    end
 end
