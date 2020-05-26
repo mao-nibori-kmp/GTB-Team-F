cefa6ddd96f6e5da1f0738a86b905b1ace012a75
uchidaryohei <ry1207ry1996@gmail.com>
2020-05-26 14:41:25 +0900

    topoftop-m ヘッダー作成 git commit -m"topoftop"-m" ヘッダー作成"

--- app/javascript/components/header_top.vue
+++ app/javascript/components/header_top.vue
@@ -1,13 +1,43 @@
 <template>
-    
+    <div class="header">
+        <div class="navbar">
+            <div class="logo">
+                <img src="/assets/レコンシェルジュロゴ白.png" alt="">
+            </div>
+            <div class="setting">
+                <img src="/assets/歯車.gif" alt="">
+            </div>
+        </div>
+    </div> 
 </template>
 
 <script>
 export default {
-  
 }
 </script>
 
 <style lang="scss" scoped>
-
+.header{
+    background-color:#666666 ;
+}
+.navbar {
+    width: calc(100% - 30px);
+    height: 60px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+}
+.setting{
+    img{
+        width: 30px;
+        height: 30px;
+    }
+}
+.logo{
+    img{
+        width:129px;
+        height:20px;    
+    }
+}
 </style>
