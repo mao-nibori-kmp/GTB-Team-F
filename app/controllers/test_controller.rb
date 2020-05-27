f1d04d313133f2ded42ce485afc0e4adc5de0ea6
tacky0612 <tacky.g25@gmail.com>
2020-05-27 12:19:07 +0900

    fix get transactions page

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -7,6 +7,7 @@ class TestController < ApplicationController
         # ここをデータベースからトークンを入手するように変更する　@user = Database.find_by(id: params[:id]) / 
         @token = "ZDk2NzJiMDVjM2JiNjVjMGI1ZWZmZmVj"
         @balance = view_context.token_to_balance(@token)
+        @transactions = view_context.token_to_transactions(@token)
     end
 
 end
